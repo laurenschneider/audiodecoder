beee0dcad8bd0da50a8dea9dc826e94dc454c75f
Lauren Braun <lbraun21@gmail.com>
2019-05-05 15:10:52 -0700

    fix loops to get decoder working

--- decode.py
+++ decode.py
@@ -10,7 +10,7 @@ from scipy.io import wavfile
 ROOT = os.path.dirname(os.path.abspath(__file__))
 DATA = os.path.join(ROOT, 'data')
 
-filepath = os.path.join(DATA, "test1.wav")
+filepath = os.path.join(DATA, "schneider.wav")
 
 # Read sample rate and data from audio file
 rate, data = wavfile.read(filepath)
@@ -30,10 +30,7 @@ space_filter.calculate_coeff()
 bit_string = ''
 
 # for all samples in numpy array of data
-for i in range(0, data.size):
-
-# testing only
-#for i in range(500):
+for i in range(data.size + 1):
 
     # for each chunk of 160 samples
     if i%160 == 0 and i != 0:
@@ -45,30 +42,21 @@ for i in range(0, data.size):
         mark_amp = mark_filter.filter(samples)
         space_amp = space_filter.filter(samples)
 
-        # for testing
-        print("mark amp:")
-        print(mark_amp)
-        print("space amp:")
-        print(space_amp)
-
         if mark_amp > space_amp:
             # bit is 1
             to_add = '1'
         else:
             # bit is zero
             to_add = '0'
-        bit_string = bit_string + to_add
-
-# for testing
-print(bit_string)
+        bit_string = to_add + bit_string
 
 # total message
 message = ''
 
-for x in range(0, len(bit_string)):
+for x in range(10, len(bit_string)+10):
     if x%10 == 0:
         start = x - 9
         end = x - 1
-        message = message + chr(int(bit_string[start:end],2))
+        message = chr(int(bit_string[start:end],2)) + message
 
 print(message)
